5bbf19f0cf340f63fe9bc25aeb1bdb7af84135fd
Trey Huffine <treyhuffine@gmail.com>
2018-02-11 12:52:31 -0500

    Initialize sources

---
+++ README.md
@@ -0,0 +1,19 @@
+# JavaScript Feed Sources
+
+[https://javascriptfeed.com](https://javascriptfeed.com)
+
+JavaScript Feed aggregates the best JavaScript, web development, and
+Node publications from around the web. We utilize RSS feeds from high quality
+content providers to bring all the best articles to one place.
+
+## Adding a Source
+
+The content of JSF is owned by the community. The types of feeds we currently support are:
+
+1. General XML feeds from around the web
+2. Feeds mapped to Medium users/publications
+3. Feeds mapped to Medium custom domains
+
+To submit a new resource, create a pull request with either the URL or Medium
+account under the appropriate data structure in `index.js`. We will review the
+content of the publication and include it if it passes the guidelines.
---
+++ index.js
@@ -0,0 +1,210 @@
+const sources = {};
+
+sources.mediumCustomPublications = [
+  {
+    url: 'https://levelup.gitconnected.com',
+    id: 'levelup.gitconnected.com',
+  },
+  {
+    url: 'https://medium.freecodecamp.com',
+    id: 'medium.freecodecamp.com',
+  },
+  {
+    url: 'https://hackernoon.com',
+    id: 'hackernoon.com',
+  },
+  {
+    url: 'https://code.likeagirl.io',
+    id: 'code.likeagirl.io',
+  },
+  {
+    url: 'https://edgecoders.com',
+    id: 'edgecoders.com',
+  },
+  {
+    url: 'https://codeburst.io',
+    id: 'codeburst.io',
+  },
+  {
+    url: 'https://medium.mybridge.co',
+    id: 'medium.mybridge.co',
+  },
+  {
+    url: 'https://dev-blog.apollodata.com',
+    id: 'dev-blog.apollodata.com',
+  },
+  {
+    url: 'https://cdb.reacttraining.com',
+    id: 'cdb.reacttraining.com',
+  },
+  {
+    url: 'https://blog.angular.io',
+    id: 'blog.angular.io',
+  },
+  {
+    url: 'https://blog.hellojs.org',
+    id: 'blog.hellojs.org',
+  },
+  {
+    url: 'https://slack.engineering',
+    id: 'slack.engineering',
+  },
+  {
+    url: 'https://medium.engineering',
+    id: 'medium.engineering',
+  },
+  {
+    url: 'https://blog.cloudboost.io',
+    id: 'blog.cloudboost.io',
+  },
+  {
+    url: 'https://blog.logrocket.com',
+    id: 'blog.logrocket.com',
+  },
+  {
+    url: 'https://blog.twitch.tv',
+    id: 'blog.twitch.tv',
+  },
+  {
+    url: 'https://code-cartoons.com',
+    id: 'code-cartoons.com',
+  },
+];
+
+sources.webFeeds = [
+  {
+    url: 'https://alligator.io/feed.xml',
+    id: 'alligator.io',
+  },
+  {
+    url: 'https://hacks.mozilla.org/feed',
+    id: 'hacks.mozilla.org',
+  },
+  {
+    url: 'https://www.quirksmode.org/blog/atom.xml',
+    id: 'quirksmode.org',
+  },
+  {
+    url: 'https://reactjs.org/feed.xml',
+    id: 'reactjs.org',
+  },
+  {
+    url: 'https://toddmotto.com/feed',
+    id: 'toddmotto.com',
+  },
+  {
+    url: 'https://davidwalsh.name/feed',
+    id: 'davidwalsh.name',
+  },
+  {
+    url: 'http://feeds.feedburner.com/2ality?format=xml',
+    id: '2ality.com',
+  },
+  {
+    url: 'https://scotch.io/feed',
+    id: 'scotch.io',
+  },
+  {
+    url: 'https://feed.dotdev.co',
+    id: 'dotdev.co',
+  },
+  {
+    url: 'https://webkit.org/blog/feed',
+    id: 'webkit.org',
+  },
+  {
+    url: 'http://feeds2.feedburner.com/ChrisCoyier',
+    id: 'chriscoyier.net',
+  },
+  {
+    url: 'https://blog.risingstack.com/rss',
+    id: 'blog.risingstack.com',
+  },
+  {
+    url: 'https://www.smashingmagazine.com/feed',
+    id: 'smashingmagazine.com',
+  },
+  {
+    url: 'http://feeds.feedburner.com/johnpapa',
+    id: 'johnpapa.net',
+  },
+  {
+    url: 'http://feeds.feedburner.com/TheJavascriptPlayground',
+    id: 'javascriptplayground.com',
+  },
+  {
+    url: 'https://derickbailey.com/feed',
+    id: 'derickbailey.com',
+  },
+  {
+    url: 'https://brendaneich.com/feed',
+    id: 'brendaneich.com',
+  },
+  {
+    url: 'https://stackfame.com/nodejs/feed',
+    id: 'stackfame.com',
+  },
+  {
+    url: 'https://blog.codinghorror.com/rss',
+    id: 'blog.codinghorror.com',
+  },
+  {
+    url: 'http://jsramblings.com/feed.xml',
+    id: 'jsramblings.com',
+  },
+  {
+    url: 'http://blog.mgechev.com/feed',
+    id: 'blog.mgechev.com',
+  },
+  {
+    url: 'https://jsnews.io/feed',
+    id: 'jsnews.io',
+  },
+  {
+    url: 'http://www.echojs.com/rss',
+    id: 'echojs.com',
+  },
+  {
+    url: 'https://react.jsnews.io/feed',
+    id: 'react.jsnews.io',
+  },
+  {
+    url: 'https://javascriptreport.com/rss',
+    id: 'javascriptreport.com',
+  },
+  {
+    url: 'http://jsdiaries.com/feed',
+    id: 'jsdiaries.com',
+  },
+  {
+    url: 'https://javascriptkicks.com/feeds/rss',
+    id: 'javascriptkicks.com',
+  },
+];
+
+sources.mediumPublications = [
+  'dailyjs',
+  'javascript-scene',
+  'dev-channel',
+  'webpack',
+  'learning-new-stuff',
+  'hackingui',
+  'the-node-js-collection',
+  'coderbyte',
+  'airbnb-engineering',
+  'front-end-hacking',
+  'statuscode',
+  'js-dojo',
+  'google-developers',
+  'on-coding',
+];
+
+sources.mediumUsers = [
+  '@RisingStack',
+  '@Pinterest_Engineering',
+  '@sachagreif',
+  '@addyosmani',
+  '@dan_abramov',
+];
+
+module.exports = sources;
